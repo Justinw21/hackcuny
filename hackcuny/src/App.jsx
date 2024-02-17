9f885ca3a669ac41e1fc94fc210881051d534cf3
NotEmily3203 <linemily62@gmail.com>
2024-02-17 17:07:21 -0500

    map restarted

--- hackcuny/src/App.jsx
+++ hackcuny/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Intro from './components/map/Intro'
+import GMap from './components/map/Map'
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import AuthDetails from './components/auth/AuthDetails';
@@ -14,7 +15,7 @@ function App() {
       <div>
         <Navbar/>
           <Routes>
-            <Route path="/" element={<Intro/>}/>
+            <Route path="/" element={<GMap/>}/>
             <Route path="/signin" element={<SignIn/>}/>
             <Route path="/signup" element={<SignUp/>}/>
           </Routes>
