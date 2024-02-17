27fc9061aeeda717af40e401748050aec650184f
NotEmily3203 <linemily62@gmail.com>
2024-02-17 11:09:42 -0500

    places suggestion

--- hackcuny/src/components/map/Intro.jsx
+++ hackcuny/src/components/map/Intro.jsx
@@ -1,31 +1,56 @@
-"use client";
-
-import { useState } from "react";
-import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  Pin,
-  InfoWindow,
-} from "@vis.gl/react-google-maps";
+import { useState, useEffect, useRef } from "react";
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import './intro.css'
+
 function Intro() {
   const position = { lat: 40.7128, lng: -74.0060 };
-  const [open, setOpen] = useState(false);
+  const [place, setPlace] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      const autocomplete = new window.google.maps.places.Autocomplete(inputRef.current, {
+        types: ['geocode'],
+        componentRestrictions: { country: 'us' }
+      });
+      autocomplete.addListener('place_changed', () => {
+        //actual place needs to be represented on map
+        const selectedPlace = autocomplete.getPlace();
+        //console.log(selectedPlace)
+      });
+    }
+  }, [inputRef]);
+
+  const handleChange = (event) => {
+    setPlace(event.target.value);
+  };
 
   return (
-    <LoadScript
+    <div className="map-container">
+      <label htmlFor="search">Enter a place: </label>
+      <input
+        type="text"
+        id="search"
+        ref={inputRef} 
+        value={place}
+        onChange={handleChange}
+        required
+      />
+      <LoadScript
         googleMapsApiKey="AIzaSyCwxPwfxk2Ln1ERsqjtPOc08-gJDIrAyBg"
+        libraries={['places']}
       >
         <GoogleMap
-          mapContainerStyle={{height: '100vh',width: '100%'}}
+          mapContainerStyle={{height: '90vh', width: '100%'}}
           zoom={13}
           center={position}
-          gestureHandling="greedy" // Set the gestureHandling property here
+          gestureHandling="greedy"
         >
-          { /* Child components, markers, etc. */ }
+          {/* Child components, markers, etc. */}
         </GoogleMap>
       </LoadScript>
+    </div>
   );
 }
-export default Intro
+
+export default Intro;
