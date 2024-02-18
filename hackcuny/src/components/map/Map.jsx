6a0dd7dd8bf6692339b3a882297470ea4b77b544
NotEmily3203 <linemily62@gmail.com>
2024-02-17 19:55:56 -0500

    pan to desired location and mark

--- hackcuny/src/components/map/Map.jsx
+++ hackcuny/src/components/map/Map.jsx
@@ -56,13 +56,27 @@ export default function GMap(){
         mapRef.current = map;
     },[]);
 
+    const panTo = React.useCallback(({ lat, lng }) => {
+        mapRef.current.panTo({ lat, lng });
+        mapRef.current.setZoom(14);
+        setMarkers((current) => [
+            ...current, 
+            {
+            lat: lat,
+            lng: lng,
+            time: new Date(),
+            },
+        ]);
+      }, []);
+
     if(loadError) return "Error loading maps"
     if(!isLoaded) return "Loading maps"
     return(
         <div>
         <h1>Pads & Tampons</h1>
 
-        <Search/>
+        <Locate panTo={panTo} />
+        <Search panTo={panTo} />
 
         <GoogleMap
         mapContainerStyle={mapContainerStyle}
@@ -96,11 +110,78 @@ export default function GMap(){
     )
 }
 
-function Search(){
-    const {ready, value, suggestions:{status, data}} = usePlacesAutocomplete({
-        requestOptions:{
-            location:{lat: ()=> 40.7128, lng: ()=> -74.0060},
-            radius:21600,
-        },
+function Locate({ panTo }) {
+    return (
+      <button
+        className="locate"
+        onClick={() => {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              panTo({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              });
+            },
+            () => null
+          );
+        }}
+      >
+        <img src="/compass.svg" alt="compass" />
+      </button>
+    );
+  }
+
+function Search({ panTo }) {
+    const {
+      ready,
+      value,
+      suggestions: { status, data },
+      setValue,
+      clearSuggestions,
+    } = usePlacesAutocomplete({
+      requestOptions: {
+        location: { lat: () => 43.6532, lng: () => -79.3832 },
+        radius: 100 * 1000,
+      },
     });
-}
+  
+    // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+  
+    const handleInput = (e) => {
+      setValue(e.target.value);
+    };
+  
+    const handleSelect = async (address) => {
+      setValue(address, false);
+      clearSuggestions();
+  
+      try {
+        const results = await getGeocode({ address });
+        const { lat, lng } = await getLatLng(results[0]);
+        panTo({ lat, lng });
+      } catch (error) {
+        console.log("😱 Error: ", error);
+      }
+    };
+  
+    return (
+      <div className="search">
+        <Combobox onSelect={handleSelect}>
+          <ComboboxInput
+            value={value}
+            onChange={handleInput}
+            disabled={!ready}
+            placeholder="Search your location"
+          />
+          <ComboboxPopover>
+            <ComboboxList>
+              {status === "OK" &&
+                data.map(({ id, description }) => (
+                  <ComboboxOption key={id} value={description} />
+                ))}
+            </ComboboxList>
+          </ComboboxPopover>
+        </Combobox>
+      </div>
+    );
+}  
